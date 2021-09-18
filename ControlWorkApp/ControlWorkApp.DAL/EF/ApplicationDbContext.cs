6b9b8f3f03ec53c14ef86b008689864a9531274a
Василий Василий <vas666@yandex.by>
2021-09-18 23:25:10 +0300

    Add WebAPI, BLL, DAL, Test projects

---
+++ ControlWorkApp/ControlWorkApp.DAL/EF/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlWorkApp.DAL.EF
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> optionsBuilder)
+            : base(optionsBuilder)
+        {
+        }
+
+        public DbSet<CompanyEntity> Companies { get; set; }
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
---
+++ ControlWorkApp/ControlWorkApp.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ControlWorkApp.DAL.Interfaces
+{
+    public interface IRepository<TEntity>
+        where TEntity : class
+    {
+        TEntity GetById(int id);
+
+        IEnumerable<TEntity> GetAll();
+
+        TEntity Create(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+    }
+}
