165621526ee7e49cb5f84d085cfe59d74b22b975
Василий Василий <vas666@yandex.by>
2021-10-22 04:09:07 +0300

    Commit

---
+++ Homework10/Homework10/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Homework10.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> optionsBuilder)
+            : base(optionsBuilder)
+        {
+        }
+
+        //private readonly IConfiguration _configuration;
+
+        //protected ApplicationDbContext(IConfiguration configuration)
+        //{
+        //    _configuration = configuration;
+        //}
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        //}
+    }
+}
